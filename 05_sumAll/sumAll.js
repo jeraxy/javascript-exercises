138b0b5a6f85bb56804844a4e7cfbc28f407bd29
jeraxy <139098109+jeraxy@users.noreply.github.com>
2023-11-29 16:57:06 -0800

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
 
+    if(num1 < 0 || num2 < 0 ||
+       typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
+
+    for (i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
