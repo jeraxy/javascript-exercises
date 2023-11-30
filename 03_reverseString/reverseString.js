ad24bf20dae11d809b76d0149c20e92eaaf9c930
jeraxy <139098109+jeraxy@users.noreply.github.com>
2023-11-29 16:34:00 -0800

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
+    let stringArray = string.split("");
 
+    for (let i = stringArray.length - 1; i >=0; i--) {
+        newString += stringArray[i];
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
