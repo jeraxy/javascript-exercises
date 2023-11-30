86f3008fd2579db0f51e72d19359e01e9675a422
jeraxy <139098109+jeraxy@users.noreply.github.com>
2023-11-29 16:47:16 -0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    newArray = [];
+    removalArray = Array.from(arguments).slice(1);
 
+    outer:
+    for (item of array){
+        for (itemToRemove of removalArray) {
+            if (item === itemToRemove) continue outer;
+        }
+        newArray.push(item);
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
